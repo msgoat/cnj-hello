ab0cbba01d530ac618fb5e55c72ec6b0d30f5470
theism <michael.theis@msg.group>
2020-05-25 13:52:47 +0200

    reset all test.target.routes to localhost

--- cnj-hello-backend-micro/run_ci_build.cmd
+++ cnj-hello-backend-micro/run_ci_build.cmd
@@ -1,7 +1,7 @@
 call mvn clean install -P commit-stage
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
-call mvn clean install -P dockerize-stage -Dtest.target.route=http://192.168.99.100:8080
+call mvn clean install -P dockerize-stage -Dtest.target.route=http://localhost:8080
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 call mvn clean verify -P integration-test-stage
