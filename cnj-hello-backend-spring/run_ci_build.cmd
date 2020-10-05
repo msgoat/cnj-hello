104ca9b8d1206bcaa8c860cb5fcf9e8c0507c1f0
theism <michael.theis@msg.group>
2020-10-05 16:38:54 +0200

    all showcases: minor upgrades and fixes
    - upgraded Payara Micro to 5.2020.4
    - upgraded Quarkus to 1.8.1
    - upgraded Spring Boot to 2.3.4

--- cnj-hello-backend-spring/run_ci_build.cmd
+++
@@ -1,8 +0,0 @@
-call mvn clean install -P commit-stage
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-call mvn clean install -P dockerize-stage -Dtest.target.route=http://localhost:8080
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
-
-call mvn verify -P integration-test-stage
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
